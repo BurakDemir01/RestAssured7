232cf7610bfbc9a8aa8bca6b45243b468aad9623
burak <burak_demi1@web.de>
2024-05-14 20:11:29 +0200

    GoRest delete Comment

--- src/test/java/GoRest/_08_GoRestCommentTest.java
+++ src/test/java/GoRest/_08_GoRestCommentTest.java
@@ -4,14 +4,15 @@ import com.github.javafaker.Faker;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 import static io.restassured.RestAssured.baseURI;
-import static io.restassured.RestAssured.given;
 
 public class _08_GoRestCommentTest {
     //     {
@@ -23,17 +24,16 @@ public class _08_GoRestCommentTest {
     //     }
 
 
-    Faker randomUreteci=new Faker();
+    Faker randomUreteci = new Faker();
     RequestSpecification reqSpec;
-    int commentID=0;
+    int commentID = 0;
 
 
     @BeforeClass
-    public void Setup()
-    {
-        baseURI="https://gorest.co.in/public/v2/comments";
+    public void Setup() {
+        baseURI = "https://gorest.co.in/public/v2/comments";
 
-        reqSpec= new RequestSpecBuilder()
+        reqSpec = new RequestSpecBuilder()
                 .addHeader("Authorization", "Bearer eb134d5bcbdf9d5d073d66aa35b8d60297f3c6a33b2039d4c2a0acac6d4b1ac7")
                 .setContentType(ContentType.JSON)
                 .build();
@@ -41,19 +41,19 @@ public class _08_GoRestCommentTest {
     // Soru : CreateComment testini yapiniz
 
     @Test
-    public void CreateComment(){
-        String fullName=randomUreteci.name().fullName();
-        String email=randomUreteci.internet().emailAddress();
-        String body=randomUreteci.lorem().paragraph();
-        String postId="122490";
+    public void CreateComment() {
+        String fullName = randomUreteci.name().fullName();
+        String email = randomUreteci.internet().emailAddress();
+        String body = randomUreteci.lorem().paragraph();
+        String postId = "122490";
 
-        Map<String,String> newComment=new HashMap<>();
-        newComment.put("name",fullName);
-        newComment.put("email",email);
-        newComment.put("body",body);
-        newComment.put("post_id",postId);
+        Map<String, String> newComment = new HashMap<>();
+        newComment.put("name", fullName);
+        newComment.put("email", email);
+        newComment.put("body", body);
+        newComment.put("post_id", postId);
 
-        commentID=
+        commentID =
                 given()
                         .spec(reqSpec)
                         .body(newComment)
@@ -68,5 +68,67 @@ public class _08_GoRestCommentTest {
         ;
 
         System.out.println("commentID = " + commentID);
+    }
+
+    @Test(dependsOnMethods = "CreateComment")
+    public void getCommentId()
+    {
+        given()
+                .spec(reqSpec)
+
+                .when()
+                .get("" + commentID)
+
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(commentID))
+        ;
+    }
+
+    @Test(dependsOnMethods = "getCommentId")
+    public void updateComment(){
+        String updName="alex de Souza";
+
+        Map<String,String> updComment=new HashMap<>();
+        updComment.put("name",updName);
+
+        given()
+                .spec(reqSpec)
+                .body(updComment)
+
+                .when()
+                .put("/"+commentID)
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("id", equalTo(commentID))
+                .body("name", equalTo(updName))
+                ;
+    }
+
+    @Test(dependsOnMethods = "updateComment")
+    public void deleteComment(){
+        given()
+                .spec(reqSpec)
+
+                .when()
+                .delete("/"+commentID)
+
+                .then()
+                // .log().body()
+                .statusCode(204);
+    }
+
+    @Test(dependsOnMethods = "deleteComment")
+    public void deleteCommentNegative(){
+        given()
+                .spec(reqSpec)
+
+                .when()
+                .delete("/"+commentID)
+
+                .then()
+                .statusCode(404);
     }
 }
